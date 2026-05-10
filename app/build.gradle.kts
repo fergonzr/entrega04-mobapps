82c4cd9837047906fd3df3461a18a48d57457cac
chariiicarrillo <zharick.carrillo@upb.edu.co>
2026-05-09 22:08:54 -0500

    add new namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.angelorphanage"
+    namespace = "com.juego.petangels"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
